a5d92d96fb92a9c0b94f114ffdc5abe1fbdecae4
Jay <jgporter87@outlook.com>
2021-05-17 10:17:46 +1000

    add form to contact page

---
+++ www/formHandeler.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const formButton = document.querySelector("form button")
+    const form = document.querySelector("form")
+    const formName = document.querySelector("#name")
+    
+
+    formButton.onclick = (e) => {
+        e.preventDefault()
+    }
+
+
+    
+})
